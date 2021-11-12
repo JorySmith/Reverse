f995112cb8ed295453fb0e00d6a1c3a466900e96
Jory <46983170+JorySmith@users.noreply.github.com>
2021-11-12 11:19:12 -0600

    Updated app demo page

--- js/site.js
+++ js/site.js
@@ -1,4 +1,29 @@
-// Call Hello World
-function helloWorld() {
-    alert("Hello World");
+// Get user inputted string aka Controller function
+function getInputString() {
+    document.getElementById("alertBox").classList.add("invisible");
+    let inputString = document.getElementById("inputString").value;
+    let revString = reverseString(inputString);
+    displayString(revString);
+}
+
+// Reverse input string aka Logic function
+function reverseString(inputString) {    
+    // Since a string can be treated as an array,
+    // use a for loop starting from the input string's end
+    // and push each letter to a new reveredString array
+    let reversedString = [];
+    for (let i = inputString.length-1; i >= 0; i--) {
+        reversedString += inputString[i];        
+    }
+    return reversedString
+}
+
+// Display reversed string in the DOM/alert message section
+// aka View function
+function displayString(revString) {
+    // Display reversed string in the DOM
+    document.getElementById("alertMsg").innerHTML = `Your string has been reversed to: <br> 
+    <span class="fw-bold">${revString}</span>`
+    // Show the alert box message
+    document.getElementById("alertBox").classList.remove("invisible");
 }
